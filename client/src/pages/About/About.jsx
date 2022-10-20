9a4c2be87daebcd20945e94c403ae417f4ff5524
Nate <itsnzte@gmail.com>
2022-10-20 13:13:59 -0700

    updated color variables

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
             </div>
 
             <div className="about-page-s1-right">
-              <img src="https://img.freepik.com/free-photo/cupcakes_74190-20.jpg?w=1380&t=st=1666295331~exp=1666295931~hmac=f4102f636a2440539a96c612eaa9b479c79dc718d521ba1d90baac95a242e95a" alt="" />
+              <img src="https://img.freepik.com/free-photo/cupcakes_74190-20.jpg?w=1380&t=st=1666295331~exp=1666295931~hmac=f4102f636a2440539a96c612eaa9b479c79dc718d521ba1d90baac95a242e95a" alt="" className="about-page-img" />
             </div>
 
           </section>
@@ -29,7 +29,7 @@ const About = () => {
           <section className="about-page-s2">
 
             <div className="about-page-s2-right">
-              <img src="https://img.freepik.com/free-photo/ingredients-cook-recipe-pink_144627-24731.jpg?w=1380&t=st=1666295389~exp=1666295989~hmac=fef1648b96b0a201051762a81feeade32e4a74f83b079a8273fbd10f23377ecb" alt="" />
+              <img src="https://img.freepik.com/free-photo/ingredients-cook-recipe-pink_144627-24731.jpg?w=1380&t=st=1666295389~exp=1666295989~hmac=fef1648b96b0a201051762a81feeade32e4a74f83b079a8273fbd10f23377ecb" alt="" className="about-page-img" />
             </div>
 
             <div className="about-page-s2-left">
