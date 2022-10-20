23df163fbdfc19b2452193958bb6402ef6921170
Nate <itsnzte@gmail.com>
2022-10-20 12:01:55 -0700

    added container class, and started about page

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
 
 // Styles
 import "./about.scss";
 
 const About = () => {
   return (
-    <div id="aboutPage">About</div>
-  )
-}
+    <div id="aboutPage">
+      <div className="container">
+
+        <h1>About Us!</h1>
+        
+        <div className="about-container">
+          <img src="" alt="" />
+        </div>
+
+      </div>
+    </div>
+  );
+};
 
 export default About;
