3603c92c91f73d20db75d8c680664dec03ef7b83
Eric <ericdoan2008@gmail.com>
2022-10-19 19:11:04 -0700

    fixed typo on name file

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -2,13 +2,13 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type Admin {
-    _id: ID
-    Adminname: String
-    email: String
+    _id: ID!
+    username: String!
+    email: String!
   }
   type Auth {
     token: ID!
-    admin: Admin
+    user: Admin
   }
   type Query {
     me: Admin
