d6474172ee044bd8bdbf1f3bbe82f0373c33121c
aq032 <anthonyq032@gmail.com>
2022-10-20 13:41:56 -0700

    Added navbar html and usestate to component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,59 @@
-import React from "react";
-
+import React, { useState } from "react";
 // Styles
 import "../sass/components/navbarComp.scss";
 
-const Navbar = () => {
+function Navbar() {
+  // State for whether the hamburger menu is active
+  const [isActive, setActive] = useState("false");
+
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
+
   return (
     <nav className="navbar">
-      Navbar
+      <a href="#" className="nav-branding">
+        Anthony Quinones
+      </a>
+
+      <ul className={`nav-menu ${isActive ? "" : "active"}`}>
+        <li className="nav-item">
+          <a onClick={handleToggle} href="#" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a onClick={handleToggle} href="#" className="nav-link">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a onClick={handleToggle} href="#" className="nav-link">
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a onClick={handleToggle} href="#" className="nav-link">
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a onClick={handleToggle} href="#" className="nav-link">
+            Resume
+          </a>
+        </li>
+      </ul>
+
+      <div
+        className={`hamburger ${isActive ? "" : "active"}`}
+        onClick={handleToggle}
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
