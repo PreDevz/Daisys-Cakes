43272663a096ac6c05ad08ed727b27931f723ffc
aq032 <anthonyq032@gmail.com>
2022-10-21 09:38:36 -0700

    Implemented react router Links to navbar. Deleted extraneous code

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Styles
 import "../sass/components/navbarComp.scss";
 import Logo from "../assets/daisylogo.png";
 
-function Navbar({ currentPage, handlePageChange }) {
+function Navbar() {
   // State for whether the hamburger menu is active
   const [isActive, setActive] = useState("false");
 
@@ -15,56 +14,54 @@ function Navbar({ currentPage, handlePageChange }) {
   };
 
   return (
-    <Router>
-      <nav className="navbar">
-        <a href="#" className="nav-branding">
-          <img id="logo" src={Logo} alt="Daisy's Cakes Logo" />
-        </a>
+    <nav className="navbar">
+      <Link to="/" className="nav-branding">
+        <img id="logo" src={Logo} alt="Daisy's Cakes Logo" />
+      </Link>
 
-        <ul className={`nav-menu ${isActive ? "" : "active"}`}>
-          <li className="nav-item">
-            <a onClick={handleToggle} href="#" className="nav-link">
-              HOME
-            </a>
-          </li>
-          <li className="nav-item">
-            <a onClick={handleToggle} href="#" className="nav-link">
-              ABOUT
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              onClick={handleToggle}
-              href="https://www.instagram.com/daisycakes87"
-              target="_blank"
-              rel="noreferrer"
-              className="nav-link"
-            >
-              INSTAGRAM
-            </a>
-          </li>
-          <li className="nav-item">
-            <a onClick={handleToggle} href="#" className="nav-link">
-              CONTACT
-            </a>
-          </li>
-          <li className="nav-item">
-            <a onClick={handleToggle} href="#" className="nav-link">
-              LOG IN
-            </a>
-          </li>
-        </ul>
+      <ul className={`nav-menu ${isActive ? "" : "active"}`}>
+        <li className="nav-item">
+          <Link onClick={handleToggle} to="/" className="nav-link">
+            HOME
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link onClick={handleToggle} to="/about" className="nav-link">
+            ABOUT
+          </Link>
+        </li>
+        <li className="nav-item">
+          <a
+            onClick={handleToggle}
+            href="https://www.instagram.com/daisycakes87"
+            target="_blank"
+            rel="noreferrer"
+            className="nav-link"
+          >
+            INSTAGRAM
+          </a>
+        </li>
+        <li className="nav-item">
+          <Link onClick={handleToggle} to="/contact" className="nav-link">
+            CONTACT
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link onClick={handleToggle} to="/admin" className="nav-link">
+            LOG IN
+          </Link>
+        </li>
+      </ul>
 
-        <div
-          className={`hamburger ${isActive ? "" : "active"}`}
-          onClick={handleToggle}
-        >
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </div>
-      </nav>
-    </Router>
+      <div
+        className={`hamburger ${isActive ? "" : "active"}`}
+        onClick={handleToggle}
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
   );
 }
 
--- client/src/pages/DaisyCakesContainer.jsx
+++ client/src/pages/DaisyCakesContainer.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 // Component
 import { Navbar, Footer } from "../components";
 
+// Router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // Pages
 import { Home, About, Contact, Admin } from "./index";
 
@@ -10,33 +13,42 @@ const DaisyCakesContainer = () => {
   const [currentPage, setCurrentPage] = useState("About");
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render. Defaults to Home if an error occurs.
-  const renderPage = () => {
-    if (currentPage === "Home") {
-      return <Home />;
-    }
-    if (currentPage === "About") {
-      return <About />;
-    }
-    if (currentPage === "Contact") {
-      return <Contact />;
-    }
-    if (currentPage === "Admin") {
-      return <Admin />;
-    }
-    return <Home />;
-  };
+  // const renderPage = () => {
+  //   if (currentPage === "Home") {
+  //     return <Home />;
+  //   }
+  //   if (currentPage === "About") {
+  //     return <About />;
+  //   }
+  //   if (currentPage === "Contact") {
+  //     return <Contact />;
+  //   }
+  //   if (currentPage === "Admin") {
+  //     return <Admin />;
+  //   }
+  //   return <Home />;
+  // };
 
   // Sets the States
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <React.StrictMode>
-      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-
+      {/* <Navbar currentPage={currentPage} handlePageChange={handlePageChange} /> */}
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </main>
       {/* Here we are calling the renderPage method which will return a component  */}
-      {renderPage()}
+      {/* {renderPage()} */}
 
-      <Footer currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* <Footer currentPage={currentPage} handlePageChange={handlePageChange} /> */}
+      <Footer />
     </React.StrictMode>
   );
 };
