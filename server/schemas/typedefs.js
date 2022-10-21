3fe519ef983d9aa5266a07df4cf6d19fdfa89e79
Eric <ericdoan2008@gmail.com>
2022-10-20 19:26:10 -0700

    model for Cake, CakeInput, and mutation for add, remove cake

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -5,16 +5,33 @@ const typeDefs = gql`
     _id: ID!
     username: String!
     email: String!
+    title: String
+    savedCake: [Cake]
+  }
+  type Cake {
+    cakeId: String!
+    title: String!
+    description: String
+    cakeUrl: [String]
+    savedCake: [Cake]
   }
   type Auth {
     token: ID!
     user: Admin
+  }
+  input CakeInput {
+    title: String!
+    description: String
+    cakeUrl: [String]
   }
   type Query {
     me: Admin
   }
   type Mutation {
     login(email: String!, password: String!): Auth
+    addedCake(title: String!, description: String, cakeUrl: String): Admin
+    removeCake(cakeId: String!): Admin
+    addCake(input: CakeInput): Admin
   }
 `;
 
