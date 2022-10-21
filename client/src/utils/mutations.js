721d1de58ca1ba2f7d8f8f57de9789d98ff64ded
cesar-infante <cesar261110@gmail.com>
2022-10-20 21:24:01 -0700

    added comments to mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,8 @@
+/* Importing the gql function from the apollo client. */
 import { gql } from "@apollo/client";
 
+
+/* This is a GraphQL query for LOGIN_USER. */
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
