10d625416fd7eca2446802e6edfa3eb267672133
cesar-infante <cesar261110@gmail.com>
2022-10-22 16:07:15 -0700

    testing to react-router animation

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -3,9 +3,20 @@ import React from "react";
 // Styles 
 import "./contact.scss";
 
+/* Importing the motion library from framer-motion. */
+import { motion } from "framer-motion";
+
 const Contact = () => {
   return (
-    <div id="contactPage">Contact</div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.23 }}
+      exit={{ opacity: 0 }}
+      id="contactPage"
+    >
+      Contact
+    </motion.div>
   );
 };
 
--- client/src/pages/DaisyCakesContainer.jsx
+++ client/src/pages/DaisyCakesContainer.jsx
@@ -4,17 +4,22 @@ import React from "react";
 import { Navbar, Footer } from "../components";
 
 // Router
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 // Pages
 import { Home, About, Contact, Admin } from "./index";
 
+/* A library that allows for animations to be added to the page. */
+import { AnimatePresence } from "framer-motion";
+
 const DaisyCakesContainer = () => {
+  const location = useLocation();
   return (
-    <React.StrictMode>
+    // <React.StrictMode>
+    <AnimatePresence>
       <Navbar />
       <main>
-        <Routes>
+        <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
@@ -22,7 +27,8 @@ const DaisyCakesContainer = () => {
         </Routes>
       </main>
       <Footer />
-    </React.StrictMode>
+    </AnimatePresence>
+    // </React.StrictMode>
   );
 };
 
