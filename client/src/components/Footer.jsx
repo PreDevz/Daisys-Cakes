c6883781b6a4e4112cf50283ed696fd85af2fef3
Nate <itsnzte@gmail.com>
2022-10-21 21:19:08 -0700

    aligning the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,58 +8,60 @@ import Logo from "../imgs/ex-cake-logo.png";
 const Footer = () => {
   return (
     <footer className="footer-section">
+      <div className="container">
       
-      {/* Logo  */}
-      <img className="footer-logo" src={Logo} alt="" />
+        {/* Logo  */}
+        <img className="footer-logo" src={Logo} alt="" />
 
-      {/* Daisy's Cakes */}
-      <div className="footer-sec">
-        <h4>Daisy&#39;s Cakes</h4>
-        <ul className="footer-ul">
-          <li>
-            <a href="">Learn More</a>
-          </li>
-          <li>
-            <a href="">Instagram</a>
-          </li>
-          <li>
-            <a href="">herEmail@gmail.com</a>
-          </li>
-        </ul>
+        {/* Daisy's Cakes */}
+        <div className="footer-sec">
+          <h4>Daisy&#39;s Cakes</h4>
+          <ul className="footer-ul">
+            <li>
+              <a href="">Learn More</a>
+            </li>
+            <li>
+              <a href="">Instagram</a>
+            </li>
+            <li>
+              <a href="">herEmail@gmail.com</a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Page  */}
+        <div className="footer-sec">
+          <h4>Page</h4>
+          <ul className="footer-ul">
+            <li>
+              <a href="">Featured</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Extra  */}
+        <div className="footer-sec">
+          <h4>Extra</h4>
+          <ul className="footer-ul">
+            <li>
+              <a href="">list item</a>
+            </li>
+            <li>
+              <a href="">list item</a>
+            </li>
+            <li>
+              <a href="">list item</a>
+            </li>
+          </ul>
+        </div>
+        
       </div>
-
-      {/* Page  */}
-      <div className="footer-sec">
-        <h4>Page</h4>
-        <ul className="footer-ul">
-          <li>
-            <a href="">Featured</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
-        </ul>
-      </div>
-
-      {/* Extra  */}
-      <div className="footer-sec">
-        <h4>Extra</h4>
-        <ul className="footer-ul">
-          <li>
-            <a href="">list item</a>
-          </li>
-          <li>
-            <a href="">list item</a>
-          </li>
-          <li>
-            <a href="">list item</a>
-          </li>
-        </ul>
-      </div>
-
     </footer>
   );
 };
