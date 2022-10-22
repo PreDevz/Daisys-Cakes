ed3a5b4adc5b8fb50e26bd69ab7713e2a2aea51e
Eric <ericdoan2008@gmail.com>
2022-10-22 12:53:15 -0700

    input info to cakeData and logic to seed

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,1 +1,22 @@
-// Sample Text
+const db = require("../config/connection");
+const { Cake } = require("../models");
+
+const CakeData = require("./cakeData.json");
+
+db.once("open", async () => {
+  // clean database
+  await Cake.deleteMany({});
+
+  // bulk create each model
+  const cakes = await Cake.insertMany(CakeData);
+
+  for (newCake of cakes) {
+    // randomly add each class to a school
+    const tempCake = cakes[Math.floor(Math.random() * cakes.length)];
+    tempCake.cakes.push(newCake._id);
+    await tempCake.save();
+  }
+
+  console.log("all done!");
+  process.exit(0);
+});
