f3a4062f31d64732db6165ae4a56469c3475432d
Nate <itsnzte@gmail.com>
2022-10-21 20:23:40 -0700

    setting up structure for footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,25 @@ import "../sass/components/footerComp.scss";
 const Footer = () => {
   return (
     <footer className="footer-section">
-      Footer Section 
+      
+      {/* Logo  */}
+      <img src="https://drive.google.com/file/d/1Veez7K9P6NfO_99GEbT7ReJJapyZ1Mm-/view?pli=1" alt="" />
+
+      {/* Daisy's Cakes */}
+      <ul className="footer-ul">
+
+        <li className="footer-ul-li">
+          <a href="">Learn More</a>
+        </li>
+        <li className="footer-ul-li">
+          <a href="">Instagram</a>
+        </li>
+        <li className="footer-ul-li">
+          <a href="">herEmail@gmail.com</a>
+        </li>
+
+      </ul>
+
     </footer>
   );
 };
