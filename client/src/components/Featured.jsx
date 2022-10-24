ce84f6dc726bbec9f2d82c6d1328392d22357241
aq032 <anthonyq032@gmail.com>
2022-10-24 13:20:53 -0700

    Updated featured section for mobile responsiveness

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -13,9 +13,18 @@ const Featured = () => {
       <h3>Featured Cakes of the Month: October</h3>
 
       <div className="featured-page-1">
-        <img className="featured-img" src={diaMuertos} />
-        <img className="featured-img" src={darkRose} />
-        <img className="featured-img" src={cuteGhost} />
+        <div>
+          <img className="featured-img" src={diaMuertos} />
+          <p>Dia De Los Muertos</p>
+        </div>
+        <div>
+          <img className="featured-img" src={darkRose} />
+          <p>Dark Rose</p>
+        </div>
+        <div>
+          <img className="featured-img" src={cuteGhost} />
+          <p>Cute Ghost</p>
+        </div>
       </div>
     </section>
   );
