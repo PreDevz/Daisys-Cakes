aec364ba76f8c115789f23003cfe8dac5db72b5f
Nate <itsnzte@gmail.com>
2022-10-24 21:23:17 -0700

    added container on about section in homepage to fix screen scaling

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -9,23 +9,29 @@ import "../sass/components/featComp.scss";
 const Featured = () => {
   return (
     <section className="featured-section">
-      <h2>Featured</h2>
-      <h3>Featured Cakes of the Month: October</h3>
 
-      <div className="featured-page-1">
-        <div>
-          <img className="featured-img" src={diaMuertos} />
-          <p>Dia De Los Muertos</p>
+      <div className="container">
+
+        <h2>Featured</h2>
+        <h3>Featured Cakes of the Month: October</h3>
+
+        <div className="featured-page-1">
+          <div>
+            <img className="featured-img" src={diaMuertos} />
+            <p>Dia De Los Muertos</p>
+          </div>
+          <div>
+            <img className="featured-img" src={darkRose} />
+            <p>Dark Rose</p>
+          </div>
+          <div>
+            <img className="featured-img" src={cuteGhost} />
+            <p>Cute Ghost</p>
+          </div>
         </div>
-        <div>
-          <img className="featured-img" src={darkRose} />
-          <p>Dark Rose</p>
-        </div>
-        <div>
-          <img className="featured-img" src={cuteGhost} />
-          <p>Cute Ghost</p>
-        </div>
+        
       </div>
+
     </section>
   );
 };
