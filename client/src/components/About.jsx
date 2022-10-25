23b62b16a1a1bf04326eaeb6299b9df191904b01
Nate <itsnzte@gmail.com>
2022-10-24 21:30:42 -0700

    added container to about section and restyled h2s in homepage

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -7,14 +7,19 @@ import "../sass/components/aboutComp.scss";
 const About = () => {
   return (
     <section className="about-section">
-      <h2>About</h2>
-      <div className="image-wrapper">
-        <img className="about-img" src={aboutImg} />
-        <p>
-          Daisy&#39;s Cakes specializes in making custom cakes for all
-          occasions. Take a look at our gallery on instagram for more! For any
-          questions and inquires please fill out the inquire form below.
-        </p>
+
+      <div className="container">
+        
+        <h2>About</h2>
+          <div className="image-wrapper">
+            <img className="about-img" src={aboutImg} />
+            <p>
+              Daisy&#39;s Cakes specializes in making custom cakes for all
+              occasions. Take a look at our gallery on instagram for more! For any
+              questions and inquires please fill out the inquire form below.
+            </p>
+        </div>
+        
       </div>
     </section>
   );
