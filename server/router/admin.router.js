f42eab7cd1aae46789f30f0956af8fea13375cac
Eric <ericdoan2008@gmail.com>
2022-10-28 19:48:28 -0700

    Admin bro connected to Daisycakes database

---
+++ server/router/admin.router.js
@@ -0,0 +1,16 @@
+const AdminBro = require("admin-bro");
+const AdminBroExpress = require("@admin-bro/express");
+const AdminBroMongoose = require('@admin-bro/mongoose');
+
+const mongoose = require("mongoose");
+
+AdminBro.registerAdapter(AdminBroMongoose);
+
+const adminBro = new AdminBro({
+  databases: [mongoose],
+  rootPath: "/admin",
+});
+
+const router = AdminBroExpress.buildRouter(adminBro);
+
+module.exports = router
