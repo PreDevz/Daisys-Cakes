1e93461cccffee8d8a8308d119273436976d8745
Nate <itsnzte@gmail.com>
2022-10-28 21:41:30 -0700

    changed import for featured images

--- client/src/imgs/index.js
+++ client/src/imgs/index.js
@@ -1,5 +1,6 @@
-// Importing images and exporting directly 
+// Importing images and exporting directly
 
+// Contact 
 export const brownCake = require("./contact/brown-floral.jpg");
 export const lavenderCake = require("./contact/lavender-butterflies.jpg");
 export const greenCake = require("./contact/green-with-rose.jpg");
@@ -9,3 +10,8 @@ export const sandlotCake = require("./contact/sandlot-1.jpg");
 export const flamingoCake = require("./contact/flamingo.jpg");
 export const sweetCake = require("./contact/two-sweet.jpg");
 export const lavbdayCake = require("./contact/lavbday.jpg");
+
+// Featured 
+export const cuteGhost = require("../imgs/homePageCakes/featured3.jpg");
+export const diaMuertos = require("../imgs/homePageCakes/featured2.jpg");
+export const darkRose = require("../imgs/homePageCakes/featured1.jpg");
