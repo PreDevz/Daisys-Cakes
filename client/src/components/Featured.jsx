665b3a1a661f632189b8eb21eefad790f402372a
aq032 <anthonyq032@gmail.com>
2022-11-07 12:02:13 -0800

    Updated featured section

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-// Images 
-import { cuteGhost, diaMuertos, darkRose} from "../imgs";
+// Images
+import { cuteGhost, diaMuertos, darkRose } from "../imgs";
 
 // Styles
 import "../sass/components/featComp.scss";
@@ -9,29 +9,34 @@ import "../sass/components/featComp.scss";
 const Featured = () => {
   return (
     <section className="featured-section">
-
       <div className="container">
-
         <h2>Featured</h2>
-        <h3>Featured Cakes of the Month: October</h3>
+        <h3>Featured Cakes of the Month: November</h3>
 
         <div className="featured-page-1">
           <div>
-            <img className="featured-img" src={diaMuertos} />
+            <img
+              className="featured-img"
+              src="https://i.imgur.com/277gr9O.jpg"
+            />
             <p>Dia De Los Muertos</p>
           </div>
           <div>
-            <img className="featured-img" src={darkRose} />
-            <p>Dark Rose</p>
+            <img
+              className="featured-img"
+              src="https://i.imgur.com/JJtasCQ.jpg"
+            />
+            <p>Vintage Cask</p>
           </div>
           <div>
-            <img className="featured-img" src={cuteGhost} />
-            <p>Cute Ghost</p>
+            <img
+              className="featured-img"
+              src="https://i.imgur.com/iw0XnNg.jpg"
+            />
+            <p>Sid the Science Kid</p>
           </div>
         </div>
-        
       </div>
-
     </section>
   );
 };
